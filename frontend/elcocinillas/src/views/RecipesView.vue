faeebce9a5159e2256884a0e6e7c087c3b94939d
beatriz.bachs <beatriz.bachs@vasscompany.com>
2023-11-04 08:36:09 +0100

    Preparada llamada al endpoint getrecetas

--- frontend/elcocinillas/src/views/RecipesView.vue
+++ frontend/elcocinillas/src/views/RecipesView.vue
@@ -32,12 +32,16 @@
 
 <script>
 import RecipeCard from "@/components/RecipeCard.vue";
+import axios from "axios";
 export default {
   components: {RecipeCard},
+  /*props : {
+    filtros : FiltroReceta //TODO: recoger filtros del primer grupo
+  },*/
   data() {
     return {
       recipes: [
-        { id: 1, title: 'Macarrones con tomate' },
+        /*{ id: 1, title: 'Macarrones con tomate' },
         { id: 2, title: 'Arroz con huevo' },
         { id: 3, title: 'Berenjena rellena' },
         { id: 4, title: 'Berenjena rellena' },
@@ -46,8 +50,35 @@ export default {
         { id: 7, title: 'Berenjena rellena' },
         { id: 8, title: 'Berenjena rellena' },
         { id: 9, title: 'Berenjena rellena' },
-        { id: 10, title: 'Berenjena rellena' }
+        { id: 10, title: 'Berenjena rellena' }*/
       ]
+    }
+  },
+
+  created() {
+    this.getRecipesFromDB();
+  },
+
+  methods : {
+    getRecipesFromDB() {
+      const path = "http://localhost:8000/recetas/";
+      const filtro = {
+        user: null,
+        classe: null,
+        tipo: null,
+        ingredientes: [],
+        time: null,
+        dificultad: 3,
+      };
+
+      axios.get(path, { params: filtro})
+          .then((response) => {
+            console.log("metodo get_recetas() llamada OK");
+            this.recipes = response.data;
+          })
+          .catch((error) => {
+            console.error("Error fetching recipes:", error);
+          });
     }
   }
 };
