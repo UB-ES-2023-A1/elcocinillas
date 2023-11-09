59c48aaea61301d5164f51f8738fe8a0332ce1b7
mrmime123 <43407454+mrmime123@users.noreply.github.com>
2023-11-09 22:51:50 +0100

    Update vue.config.js

--- frontend/elcocinillas/vue.config.js
+++ frontend/elcocinillas/vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   runtimeCompiler: true,
-  publicPath: 'https://elcocinillas.azurewebsites.net',
+  publicPath: '/',
 });
