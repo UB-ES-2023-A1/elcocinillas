552c6cee87ef99356eb091f0f5ab34d1524b9cac
DiegoGelinckRodriguez <61191553+DiegoGelinckRodriguez@users.noreply.github.com>
2023-12-04 11:00:03 +0100

    Update App.vue

--- frontend/elcocinillas/src/App.vue
+++ frontend/elcocinillas/src/App.vue
@@ -1,14 +1,16 @@
 <template>
-  <div>
-    <NavComp></NavComp>
-    <router-view />
+  <div style="position: flex; direction: column">
+    <NavComp/>
+    <router-view/>
+    <FooterComp/>
   </div>
 </template>
 
 <script>
 import NavComp from "./components/NavComp.vue";
+import FooterComp from "./components/FooterComp.vue";
 
 export default {
-  components: { NavComp },
+  components: { NavComp, FooterComp },
 };
 </script>
