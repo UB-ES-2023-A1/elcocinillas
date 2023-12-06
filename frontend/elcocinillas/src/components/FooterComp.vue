fc4a2a57554c3c4a0db9d67381ca64d6fc69a804
DiegoGelinckRodriguez <61191553+DiegoGelinckRodriguez@users.noreply.github.com>
2023-12-06 22:32:20 +0100

    Update FooterComp.vue

--- frontend/elcocinillas/src/components/FooterComp.vue
+++ frontend/elcocinillas/src/components/FooterComp.vue
@@ -1,32 +1,32 @@
 <template>
     <div id="footer" style="clear:both;">
         <div class="social-media-foot">
-            <a target="_blank" title="Facebook El Cocinillas" href="http://www.facebook.com/ElCocinillas">
+            <h1 target="_blank" title="Facebook El Cocinillas" href="http://www.facebook.com/ElCocinillas">
                 <span class="elcoci-facebook" aria-hidden="true"></span>
-            </a>
-            <a target="_blank" title="Twitter El Cocinillas" href="http://www.twitter.com/elcocinillas">
+            </h1>
+            <h1 target="_blank" title="Twitter El Cocinillas" href="http://www.twitter.com/elcocinillas">
                 <span class="elcoci-twitter" aria-hidden="true"></span>
-            </a>
-            <a target="_blank" title="Instagram El Cocinillas" href="https://www.instagram.com/elcocinillas/">
+            </h1>
+            <h1 target="_blank" title="Instagram El Cocinillas" href="https://www.instagram.com/elcocinillas/">
                 <span class="elcoci-instagram" aria-hidden="true"></span>
-            </a>
-            <a target="_blank" title="Youtube El Cocinillas" href="https://www.youtube.com/user/ElCocinillas">
+            </h1>
+            <h1 target="_blank" title="Youtube El Cocinillas" href="https://www.youtube.com/user/ElCocinillas">
                 <span class="elcoci-youtube" aria-hidden="true"></span>
-            </a>
-            <a target="_blank" title="RSS El Cocinillas" href="/en/rss/">
+            </h1>
+            <h1 target="_blank" title="RSS El Cocinillas" href="/en/rss/">
                 <span class="elcoci-rss" aria-hidden="true"></span>
-            </a>
+            </h1>
         </div>
         <div style="padding: 10px; color: #fff; text-align: center;">
-            <a href="/about/">Acerca de</a> | <a href="/more/customer-service/">Atención al Cliente</a> | <a href="/more/elcocinillas/">Zona Profesional</a> | <a href="/more/contact">Contacto</a>
+            <h1 href="/about/">Acerca de</h1> | <h1 href="/more/customer-service/">Atención al Cliente</h1> | <h1 href="/more/elcocinillas/">Zona Profesional</h1> | <h1 href="/more/contact">Contacto</h1>
         </div>
         <div style="font-size: x-small; padding: 5px; color: #fff; text-align: center;">
-            <a href="/more/privacy-notice/">Privacidad</a> |
-            <a href="https://www.iubenda.com/privacy-policy/37587182/cookie-policy">Política de Cookies</a> |
-            <a class="iubenda-cs-preferences-link" href="#" data-iub-enabled="1">Preferencias de Rastreo</a> |
-            <a href="javascript:void(0)" class="iubenda-ccpa-opt-out">Do Not Sell or Share My Personal Information</a> |
-            <a href="/more/terms-of-use/">Términos de Uso</a> |
-            <a href="/more/legal-information/">Información Legal</a> |
+            <h1 href="/more/privacy-notice/">Privacidad</h1> |
+            <h1 href="https://www.iubenda.com/privacy-policy/37587182/cookie-policy">Política de Cookies</h1> |
+            <h1 class="iubenda-cs-preferences-link" href="#" data-iub-enabled="1">Preferencias de Rastreo</h1> |
+            <h1 href="javascript:void(0)" class="iubenda-ccpa-opt-out">Do Not Sell or Share My Personal Information</h1> |
+            <h1 href="/more/terms-of-use/">Términos de Uso</h1> |
+            <h1 href="/more/legal-information/">Información Legal</h1> |
         </div>
         <div style="text-align: center; padding-bottom: 20px;">
             <p>© 2023 El Cocinillas es propiedad de El Cocinillas inc.</p>
