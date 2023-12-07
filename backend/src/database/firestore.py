56e42f554d9bc2c0e7aa67621510ad75abb91b21
mrmime123 <43407454+mrmime123@users.noreply.github.com>
2023-12-07 16:50:24 +0100

    hofix

--- backend/src/database/firestore.py
+++ backend/src/database/firestore.py
@@ -254,8 +254,8 @@ def unsave_recipe(user,recipe):
     if doc.exists:
         lista = doc.get("Recetas")
         if recipe in lista:
-        lista.remove(recipe)
-        doc_ref.update({"Recetas": lista})
+            lista.remove(recipe)
+            doc_ref.update({"Recetas": lista})
     
       
 def add_comment(comment):
