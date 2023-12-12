5fcbabfab110d8438a271e6712aa167dcb3c7aa8
MinicobosGap <augustoasier10@gmail.com>
2023-12-12 18:25:34 +0100

    tests de dejar de guardar receta hechos, recordar que hace falta cambiar la comprobacion de si existe la receta o el usuario que quiere eliminarla antes de hacerlo

---
+++ backend/tests/test_valido_put_dejar_guardar_receta.py
@@ -0,0 +1,10 @@
+import requests
+
+API_URL = "http://127.0.0.1:8000"
+
+#TESTS CORRECTOS:
+def test_put_dejar_guardar_receta_valid1(): #test con receta incorrecta
+    user = "kobos" 
+    receta = "Pato Pekin" #no existe
+    response = requests.put(f"{API_URL}/dejar_de_guardar/{user}/{receta}/")
+    assert "200" in response.text
