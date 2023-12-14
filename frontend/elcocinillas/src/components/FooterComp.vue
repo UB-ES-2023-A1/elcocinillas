bdcd954e718ee3263fb67c0954eb3ccebe85652f
DiegoGelinckRodriguez <61191553+DiegoGelinckRodriguez@users.noreply.github.com>
2023-12-14 12:51:04 +0100

    Update FooterComp.vue

--- frontend/elcocinillas/src/components/FooterComp.vue
+++ frontend/elcocinillas/src/components/FooterComp.vue
@@ -1,38 +1,12 @@
 <template>
-    <div id="footer" style="clear:both;">
-        <div class="social-media-foot">
-            <h1 target="_blank" title="Facebook El Cocinillas" href="http://www.facebook.com/ElCocinillas">
-                <span class="elcoci-facebook" aria-hidden="true"></span>
-            </h1>
-            <h1 target="_blank" title="Twitter El Cocinillas" href="http://www.twitter.com/elcocinillas">
-                <span class="elcoci-twitter" aria-hidden="true"></span>
-            </h1>
-            <h1 target="_blank" title="Instagram El Cocinillas" href="https://www.instagram.com/elcocinillas/">
-                <span class="elcoci-instagram" aria-hidden="true"></span>
-            </h1>
-            <h1 target="_blank" title="Youtube El Cocinillas" href="https://www.youtube.com/user/ElCocinillas">
-                <span class="elcoci-youtube" aria-hidden="true"></span>
-            </h1>
-            <h1 target="_blank" title="RSS El Cocinillas" href="/en/rss/">
-                <span class="elcoci-rss" aria-hidden="true"></span>
-            </h1>
-        </div>
-        <div style="padding: 10px; color: #76DED9; text-align: center;">
-            <h1 href="/about/">Acerca de</h1> | <h1 href="/more/customer-service/">Atención al Cliente</h1> | <h1 href="/more/elcocinillas/">Zona Profesional</h1> | <h1 href="/more/contact">Contacto</h1>
-        </div>
-        <div style="font-size: x-small; padding: 5px; color: #76DED9; text-align: center;">
-            <h1 href="/more/privacy-notice/">Privacidad</h1> |
-            <h1 href="https://www.iubenda.com/privacy-policy/37587182/cookie-policy">Política de Cookies</h1> |
-            <h1 class="iubenda-cs-preferences-link" href="#" data-iub-enabled="1">Preferencias de Rastreo</h1> |
-            <h1 href="javascript:void(0)" class="iubenda-ccpa-opt-out">Do Not Sell or Share My Personal Information</h1> |
-            <h1 href="/more/terms-of-use/">Términos de Uso</h1> |
-            <h1 href="/more/legal-information/">Información Legal</h1> |
-        </div>
-        <div style="text-align: center; padding-bottom: 20px;">
-            <p>© 2023 El Cocinillas es propiedad de El Cocinillas inc.</p>
-        </div>
-        <div style="clear: both;"></div>
+  <div id="footer">
+    <div id="views">
+      <div class="view" v-for="v in views" v-bind:key="v">
+        <h1 @click="view(v.id)">{{ v.name }}</h1> | 
+      </div>
     </div>
+    <p>© 2023 El Cocinillas es propiedad de El Cocinillas inc.</p>
+  </div>
 </template>
 
 <style scoped>
@@ -41,6 +15,47 @@
 }
 #footer{
     background-color: #73694f;
-    padding-top: 4vh;
+    text-align: center;
+    padding-top: 2vh;
+    padding-bottom: 2vh;
+}
+#views{
+  margin: auto;
+  width: 60vh;
+}
+.view{
+  display: inline-block;
+}
+h1{
+    display: inline;
+    font-size: 2vh;
+    cursor: pointer;
+}
+h1:hover{
+  color: black;
+}
+p{
+  font-size: 1.5vh;
+  padding-top: 3vh;
 }
 </style>
+
+<script>
+import info from '../info.json'
+
+export default {
+  data(){
+    return {
+      views: info.views,
+    }
+  },
+  methods: {
+    view(v){
+      const path = '/info/' + v;
+      if(this.$router.history.current.path != path) {
+        this.$router.push(path);
+      }
+    }
+  }
+}
+</script>
